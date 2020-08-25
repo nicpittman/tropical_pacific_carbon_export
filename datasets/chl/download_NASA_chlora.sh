f6a75988a599d3f36ecb7690d562835078f3fe4a
Nic Pittman <npittman@utas.edu.au>
2020-08-25 16:58:48 +1000

    Shell script to download NASA chl data

---
+++ datasets/chl/download_NASA_chlora.sh
@@ -0,0 +1,29 @@
+# Follow NASA instructions to make the authorisation cookie
+# https://oceancolor.gsfc.nasa.gov/data/download_methods/
+
+# echo "machine urs.earthdata.nasa.gov login USERNAME password PASSWD" > ~/.netrc ; > ~/.urs_cookies
+# chmod  0600 ~/.netrc
+
+#Download Seawifs file urls to seawifs.txt
+wget -q --post-data="sensor=seawifs&sdate=1997-09-01&edate=2010-12-01&dtype=L3M&addurl=1&results_as_file=1&search=*DAY_CHL_chlor_a_9km*" -O - https://oceandata.sci.gsfc.nasa.gov/api/file_search > seawifs.txt
+
+#Download MODIS file urls to modis.txt
+wget -q --post-data="sensor=modis_aqua&sdate=2002-01-01&edate=2020-12-01&dtype=L3M&addurl=1&results_as_file=1&search=*DAY_CHL_chlor_a_9km*" -O - https://oceandata.sci.gsfc.nasa.gov/api/file_search > modis.txt
+
+#Make our dir 
+mkdir chlor_a
+mkdir chlor_a/seawifs
+mkdir chlora/modis
+
+echo 'Downloading Seawifs'
+for url in $(cat seawifs.txt); do
+    curl -O -b  ~/.urs_cookies -c ~/.urs_cookies -L -n $url
+done
+mv *S*nc chlor_a/seawifs/
+
+echo 'Downloading Modis'
+for url in $(cat modis.txt); do
+    curl -O -b  ~/.urs_cookies -c ~/.urs_cookies -L -n $url
+done
+mv *A*nc chlor_a/modis/
+
